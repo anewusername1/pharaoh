eeb92f82cfd64f638813e938a9b4ffaff7449cc3
narshlob <traceyeubanks@yahoo.com>
2010-12-22 22:19:29 -0700

    initil

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iblankwhen::Application.config.secret_token = 'cce8752bb90396182334afb45ed8877c9d02c0f12e9c9106cae80807d6240b1a8ce12f8365d89960bd35f461d93f0a82633db6ec22400bc65ca55a2b6ef5964d'
