38da11e984e3caee54366c79f016146bffe68287
Hashrocket Workstation <dev@hashrocket.com>
2011-02-10 13:22:10 -0700

    updated the paths and web_ext_steps and env.rb feature support stuff

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -10,7 +10,8 @@ module NavigationHelpers
 
     when /the home\s?page/
       '/'
-
+    when /the post page/
+      '/posts'
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
     #
